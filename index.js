3ff160d31d37752df6cc155438b6ee3686584d7b
Shipu8644 <shipu.monjurur@gmail.com>
2021-11-11 09:48:53 +0600

    all services loaded

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
+const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 5000
 
 // middleWare
 app.use(cors())
 app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.om5y9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+async function run() {
+    try {
+        await client.connect();
+        console.log("Database Connected Sucessfully");
+        const database = client.db("squa_drone");
+        const serviceCollection = database.collection("services");
+
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find({});
+            const services = await cursor.toArray();
+            res.json(services);
+        })
+
+
+
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Welcome to Drone Zone!')
